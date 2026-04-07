f0c0a3cbf76a21ff241399fb2a3ed6d89d31f1e6
Shelby Hall <shelby.hall@jrvs.ca>
2026-04-07 00:33:49 +0000

    modified psql_docker deleted fake script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,55 +1,53 @@
-#!/bin/sh
+#! /bin/bash
 
-# Capture CLI arguments
 cmd=$1
 db_username=$2
 db_password=$3
 
-# Start docker
-# Make sure you understand the double pipe operator
-sudo systemctl status docker || systemctl #todo
+sudo systemctl status docker || sudo systemctl start docker
 
-# Check container status (try the following cmds on terminal)
 docker container inspect jrvs-psql
 container_status=$?
 
-# User switch case to handle create|stop|start opetions
-case $cmd in 
-  create)
-  
-  # Check if the container is already created
-  if [ $container_status -eq 0 ]; then
-		echo 'Container already exists'
-		exit 1	
+case $cmd in
+
+	create)
+	if [ $container_status -eq 0 ]; then
+		echo "Container already exists"
+		exit 1
 	fi
 
-  # Check # of CLI arguments
-  if [ $# -ne 3 ]; then
-    echo 'Create requires username and password'
-    exit 1
-  fi
-  
-  # Create container
-	docker volume #todo
-  # Start the container
-	docker run #todo
-  # Make sure you understand what's `$?`
+	if [ $# -ne 3 ]; then
+		echo "Create requires username and password"
+		exit 1
+	fi
+
+	docker volume create pgdata
+
+docker run --name jrvs-psql \
+  -e PGPASSWORD=$db_password \
+  -e PGUSER=$db_username \
+  -d \
+  -v pgdata:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  postgres:9.6-alpine
+
+	exit$?
+	;;
+
+start|stop)
+	if [ $container_status -ne 0 ]; then
+		echo "Container has not been created"
+		exit 1
+	fi
+
+	docker container $cmd jrvs-psql
 	exit $?
 	;;
 
-  start|stop) 
-  # Check instance status; exit 1 if container has not been created
-  if [ $container_status # ... todo
-  # ... todo
-
-  # Start or stop the container
-	docker container $cmd jrvs-psql
-	exit $?
-	;;	
-  
-  *)
-	echo 'Illegal command'
-	echo 'Commands: start|stop|create'
+*)
+	echo "Illegal command"
+	echo "Commands: start|stop|create"
 	exit 1
 	;;
 esac
